8640ec6bf9d5eae02014caeca224e10b1892281c
NataliiaKyrykovych <120452030+NataliiaKyrykovych@users.noreply.github.com>
2023-08-24 18:58:41 -0500

    corrected phonebook

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,18 @@
-import { nanoid } from 'nanoid';
-import { StyledContactList } from './ContactList.styled';
+import { DeleteBtn, List, ListItem, Marker } from './ContactList.styled';
 
 export const ContactList = ({ contacts, onDelete }) => {
   return (
-    <StyledContactList>
+    <List>
       {contacts.map(contact => (
-        <li key={nanoid()}>{contact.name}: {contact.number}
-        <button onClick={() => onDelete(contact.id)}>Delete</button>
-        </li>
+        <ListItem key={contact.id}>
+          <Marker></Marker>
+          <p>
+            {contact.name}: {contact.number}
+          </p>
+
+          <DeleteBtn onClick={() => onDelete(contact.id)}>Delete</DeleteBtn>
+        </ListItem>
       ))}
-    </StyledContactList>
+    </List>
   );
 };
--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -1,14 +1,37 @@
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 
-export const StyledContactList = styled.ul`
-  li {
-    padding: 5px 10px;
-  }
+export const List = styled.ul`
+  list-style: disc;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
 
-  button {
-    margin-left: 10px;
-    font-size: 10px;
-    background-color: lightyellow;
-  }
+export const ListItem = styled.li`
+  display: flex;
+  align-items: center;
+  /* justify-content: space-between; */
+  gap: 16px;
+`;
+
+export const Marker = styled.div`
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: #000;
+`;
+
+export const DeleteBtn = styled.button`
+  margin-left: auto;
+  padding: 4px 8px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  background-color: #fff;
+  border-color: transparent;
+  width: fit-content;
   
+
+  &:hover,
+  &:focus {
+    box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 3px;
+  }
 `;
